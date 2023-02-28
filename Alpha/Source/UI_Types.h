fa5ce4d02505a90807ccaaa61888f1fbd56e775d
BearCustomerService <a.nalpon@digipen.edu>
2023-02-28 14:36:35 +0800

    Update UI_Types.h
    Updated types of UI

--- Alpha/Source/UI_Types.h
+++ Alpha/Source/UI_Types.h
@@ -5,6 +5,7 @@ namespace UI
 	enum BUTTON_TYPE {
 		WHITE_BUTTON,
 		END_PHASE_BUTTON,
+		BUILD_TOWER_BUTTON,
 		NUM_BUTTONS
 	};
 	enum UI_MESH
@@ -16,6 +17,8 @@ namespace UI
 	{
 		TEX_BUTTON,
 		TEX_END_PHASE,
+		TEX_NEXUS,
+		TEX_TOWER,
 		NUM_TEX
 	};
 }
