a140888c2fec44b12409ae932b71c73037ef0fa8
AlwinMoo <geraldcheu@gmail.com>
2023-03-01 20:33:00 +0800

    fixed crashing

--- Alpha/Source/Alwintest.h
+++ Alpha/Source/Alwintest.h
@@ -16,31 +16,3 @@ void Alwintest_Free();
 
 void Alwintest_Unload();
 
-namespace
-{
-	std::vector<GameObject*> go_list;
-	game_map* test_map;
-	int object_count;
-	GameObject* hoverStructure;
-	bool validPlacement;
-	UI::UI_Manager* uiManager;
-
-	// Textures
-	AEGfxTexture* turretTex;
-	AEGfxTexture* grassTex;
-	AEGfxTexture* nexusTex;
-	AEGfxTexture* wallTex;
-	AEGfxTexture* eraseTex;
-	AEGfxTexture* grassBorderlessTex;
-	AEGfxTexture* bulletTex;
-	AEGfxTexture* playerTex;
-	AEGfxTexture* enemyTex;
-	AEGfxTexture* texTest;
-	AEGfxVertexList* meshTest;
-
-	UI::Button* nexusButton;
-	bool nexusPlaced;
-	bool buildPhase;
-
-	int buildResource;
-}
